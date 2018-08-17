8e5d630b8f2e12739a5f4a3e5f3dc4e44fd8971c
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-08-17 04:04:05 +0000

    Disable Thermal-engine [1/2]

--- product/qcom-thermal.mk
+++ product/qcom-thermal.mk
@@ -1,8 +1,3 @@
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
-
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
