3f9ba85d85f334f39128f989af88d98a8610351f
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-08-17 23:52:42 +0000

    Some flag changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,18 +22,10 @@ TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
 
 # Inherit from proprietary files
 include vendor/yu/tomato/BoardConfigVendor.mk
-ALLOW_MISSING_DEPENDENCIES =true
+#ALLOW_MISSING_DEPENDENCIES =true
 WITH_DEXPREOPT := false
 
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-#TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+QCOM_HARDWARE_VARIANT := msm8916
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_USES_QCOM_MM_AUDIO := true
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-#TARGET_2ND_CPU_ABI :=
-#TARGET_2ND_CPU_ABI2 :=
-TARGET_2ND_CPU_VARIANT := cortex-a53
