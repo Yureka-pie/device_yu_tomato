431cb8830350a0f8321efef688e5e7946aa5bad8
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-09-08 16:30:58 +0000

    Remove Fragments

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,35 +15,88 @@
 
 include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
-include device/yu/tomato/board/*.mk
+#Audio
+TARGET_USES_QCOM_MM_AUDIO := true
 
 # Assertions
 TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
 
-# Inherit from proprietary files
-include vendor/yu/tomato/BoardConfigVendor.mk
-#ALLOW_MISSING_DEPENDENCIES =true
-WITH_DEXPREOPT := false
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/yu/tomato/bluetooth
 
-QCOM_HARDWARE_VARIANT := msm8916
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_USES_QCOM_MM_AUDIO := true
-
-# Shims
-TARGET_LD_SHIM_LIBS += \
-    /system/vendor/lib64/lib-imsdpl.so|libshims_boringssl.so \
-    /system/vendor/lib64/lib-imscamera.so|libshims_camera.so \
-    /system/vendor/lib64/lib-imsvt.so|libshims_ims.so \
-
-TARGET_QCOM_WLAN_VARIANT := wlan-caf
-
-#Telephony
-PRODUCT_PACKAGES += qti-telephony-common
-PRODUCT_BOOT_JARS += telephony-ext
-
-#mm-qcamera-deamon
+# Camera
+BOARD_CAMERA_SENSORS := imx135_cp8675 imx214_cp8675 ov5648_cp8675
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_DESTROYED_MUTEX_USAGE_WHITELIST := mm-qcamera-daemon
 
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
 	/system/bin/mediaserver=23 \
 	/system/vendor/bin/mm-qcamera-daemon=23
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 10485760
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13576175616 # 13576192000 - 16384
+
+# Use mke2fs instead of make_ext4fs
+TARGET_USES_MKE2FS := true
+
+# GPS
+TARGET_NO_RPC := true
+USE_DEVICE_SPECIFIC_GPS := true
+
+# Properties
+TARGET_SYSTEM_PROP += device/yu/tomato/system.prop
+
+# HIDL
+DEVICE_MANIFEST_FILE := device/yu/tomato/manifest.xml
+
+# Init
+TARGET_LIBINIT_MSM8916_DEFINES_FILE := device/yu/tomato/init/init_tomato.cpp
+
+# Kernel
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DT := true
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+KERNEL_TOOLCHAIN := /home/sanchit/linaro7/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+QCOM_HARDWARE_VARIANT := msm8916
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Lights
+BOARD_LIGHTS_VARIANT := aw2013
+TARGET_PROVIDES_LIBLIGHT := true
+
+#Media
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# PowerHAL
+TARGET_POWER_SET_FEATURE_LIB := libpower_set_feature_tomato
+
+# Power
+TARGET_PROVIDES_POWERHAL := true
+
+#Wifi
+TARGET_PROVIDES_WCNSS_QMI := true
+TARGET_QCOM_WLAN_VARIANT := wlan-caf
+
+# Recovery
+TARGET_RECOVERY_DENSITY := xhdpi
+TARGET_RECOVERY_FSTAB := device/yu/tomato/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+
+# Widevine
+BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 3
+
+# Inherit from proprietary files
+include vendor/yu/tomato/BoardConfigVendor.mk
